f4446068f87605f5cdc212f08ca608b47fb53635
Orisakwe Nwokocha <orisakwenwokocha1@gmail.com>
2024-04-05 08:08:34 +0100

    refactored view post response

--- src/main/java/africa/semicolon/aprokoBlog/dtos/responses/ViewPostResponse.java
+++ src/main/java/africa/semicolon/aprokoBlog/dtos/responses/ViewPostResponse.java
@@ -1,10 +1,10 @@
 package africa.semicolon.aprokoBlog.dtos.responses;
 
-import africa.semicolon.aprokoBlog.data.models.Post;
 import lombok.Data;
 
 @Data
 public class ViewPostResponse {
+    private String post;
     private String viewerId;
     private String viewer;
     private String timeOfView;
